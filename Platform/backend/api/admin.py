073fa01cf6f8c63d692b2eb1d4b3025a8115475b
Akezhan Amanzhol <amanzholakezhan897@gmail.com>
2026-04-17 21:13:51 +0500

    Delete Platform/backend directory

--- Platform/backend/api/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Profile, Category, Course, Enrollment, Task, Submission
-
-admin.site.register(Profile)
-admin.site.register(Category)
-admin.site.register(Course)
-admin.site.register(Enrollment)
-admin.site.register(Task)
-admin.site.register(Submission)
