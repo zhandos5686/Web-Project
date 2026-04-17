c593065e4208f38b13a914690ecb4e6974d62017
Akezhan Amanzhol <amanzholakezhan897@gmail.com>
2026-04-17 21:14:06 +0500

    Delete Platform/frontend directory

--- Platform/frontend/client/src/app/interfaces/profile.ts
+++
@@ -1,8 +0,0 @@
-export interface Profile {
-  id: number;
-  user: number;
-  full_name: string;
-  phone: string;
-  grade_level: string;
-  bio: string;
-}
--- Platform/frontend/client/src/app/interfaces/task.ts
+++
@@ -1,9 +0,0 @@
-export interface Task {
-  id: number;
-  course: number;
-  title: string;
-  description: string;
-  due_date: string;
-  created_by: number;
-  is_completed: boolean;
-}
